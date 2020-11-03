35629ad1725d82294e1c75112946b93ee7fc0493
YufeiT <tao.yufei@icloud.com>
2020-11-02 17:44:39 -0800

    Drone set up, now it can move around and shout if it's on flooded areas

--- src/team4player/DeliveryDrone.java
+++ src/team4player/DeliveryDrone.java
@@ -1,6 +1,10 @@
 package team4player;
+
 import battlecode.common.*;
 
+import java.util.Arrays;
+
+import static team4player.Util.directions;
 import static team4player.Util.randomDirection;
 
 public class DeliveryDrone extends Unit{
@@ -10,15 +14,23 @@ public class DeliveryDrone extends Unit{
     }
 
     public void takeTurn() throws GameActionException{
+        MapLocation loc = rc.getLocation();
         Team enemy = rc.getTeam().opponent();
-        if(!rc.isCurrentlyHoldingUnit()){
+        if (!rc.isCurrentlyHoldingUnit()) {
             nav.tryMove(randomDirection());
+            System.out.println("nothing" + Arrays.toString(rc.senseNearbyRobots(5, enemy)));
+            if(rc.senseFlooding(loc))
+                System.out.println("This place is flooded.");
         }
-//        if(!comms.broadcastedCreation){
-//            comms.broadcastDeliveryDroneCreation(rc.getLocation());
-//        }
-//        Team enemy = rc.getTeam().opponent();
-//        if (!rc.isCurrentlyHoldingUnit()) {
+
+        if(rc.isCurrentlyHoldingUnit()){
+            nav.tryMove(randomDirection());
+            if(rc.senseFlooding(loc))
+                System.out.println("This place is flooded.");
+        }
+
+
+
 //            // See if there are any enemy robots within capturing range
 //            RobotInfo[] robots = rc.senseNearbyRobots(GameConstants.DELIVERY_DRONE_PICKUP_RADIUS_SQUARED, enemy);
 //
@@ -29,7 +41,7 @@ public class DeliveryDrone extends Unit{
 //            }
 //        } else {
 //            // No close robots, so search for robots within sight radius
-//            tryMove(randomDirection());
+//            nav.tryMove(randomDirection());
 //        }
     }
 }
