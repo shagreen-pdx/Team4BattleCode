4e3a9685e5deb444c2b617269ea3936377599066
YufeiT <tao.yufei@icloud.com>
2020-11-04 19:34:32 -0800

    Added DesignSchoolTest, HQTest, MinerTest and UnitTest.

---
+++ test/team4player/UnitTest.java
@@ -0,0 +1,54 @@
+package team4player;
+
+import battlecode.common.Direction;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+public class UnitTest {
+  //Field enemyHqLoc of type MapLocation - was not mocked since Mockito doesn't mock a Final class when 'mock-maker-inline' option is not set
+  //Field hqLoc of type MapLocation - was not mocked since Mockito doesn't mock a Final class when 'mock-maker-inline' option is not set
+  @Mock
+  Navigation nav;
+  @Mock
+  ArrayList<MapLocation> posEnemyHqLoc;
+  @Mock
+  ArrayList<Integer> teamMessages;
+  @Mock
+  RobotController rc;
+  @Mock
+  Communications comms;
+  @InjectMocks
+  Unit unit;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void testTakeTurn() throws Exception {
+    unit.takeTurn();
+  }
+
+  @Test (expected = NullPointerException.class)
+  public void testTryBuildBuilding() throws Exception {
+    boolean result = unit.tryBuildBuilding(RobotType.HQ, Direction.NORTH);
+    Assert.assertEquals(true, result);
+  }
+
+  @Test
+  public void testCalcPosEnemyHqLoc() throws Exception {
+    unit.calcPosEnemyHqLoc();
+  }
+}
