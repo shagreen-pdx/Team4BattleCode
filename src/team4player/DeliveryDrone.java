586421730051c9d982e4d46dd8cd9fca7b4b55e1
Jordan Tippetts <jjtippetts@gmail.com>
2020-11-12 20:12:46 -0800

    Get flooded locations from blockchain as well as protect hq in the general direction to the enemy

--- src/team4player/DeliveryDrone.java
+++ src/team4player/DeliveryDrone.java
@@ -4,14 +4,15 @@ import battlecode.common.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 import static team4player.Util.directions;
 import static team4player.Util.randomDirection;
 
 public class DeliveryDrone extends Unit{
 
+    MapLocation locEnemyBot = null;
     ArrayList<MapLocation> refineryLocations = new ArrayList<MapLocation>();
-    ArrayList<MapLocation> floodedLocations = new ArrayList<MapLocation>();
     int currentlyHeldRobotId = 0;
     boolean haveEnemyBot = false;
     boolean search = false;
@@ -25,6 +26,11 @@ public class DeliveryDrone extends Unit{
 
     public void takeTurn() throws GameActionException{
         super.takeTurn();
+
+        // Calculate all possible enemy locations
+        if(posEnemyHqLoc.isEmpty()){
+            calcPosEnemyHqLoc();
+        }
 
         // Decipher all blockchain messages. Only happens once.
         if(!teamMessagesSearched){
@@ -73,10 +79,7 @@ public class DeliveryDrone extends Unit{
 
         } else if(search){
             System.out.println("Search for hq");
-            // Calculate all possible enemey locations
-            if(posEnemyHqLoc.isEmpty()){
-                calcPosEnemyHqLoc();
-            }
+
             // Pick up miner before traveling to enemy hq
             if (!rc.isCurrentlyHoldingUnit()) {
                 RobotInfo[] robots = rc.senseNearbyRobots(GameConstants.DELIVERY_DRONE_PICKUP_RADIUS_SQUARED, rc.getTeam());
@@ -114,7 +117,7 @@ public class DeliveryDrone extends Unit{
 
         } else {
             System.out.println("Protect Hq");
-            if(!rc.getLocation().isWithinDistanceSquared(hqLoc, 9)){
+            if(!rc.getLocation().isWithinDistanceSquared(hqLoc, 20)){
                 nav.flyTo(hqLoc);
             }
             pickupEnemyBots();
@@ -161,19 +164,38 @@ public class DeliveryDrone extends Unit{
 
     public void pickupEnemyBots() throws GameActionException {
         if (!rc.isCurrentlyHoldingUnit()) {
-            RobotInfo[] robots = rc.senseNearbyRobots(GameConstants.DELIVERY_DRONE_PICKUP_RADIUS_SQUARED, rc.getTeam().opponent());
+            RobotInfo[] robots = rc.senseNearbyRobots(20, rc.getTeam().opponent());
             System.out.println(robots.length);
             if (robots.length > 0) {
                 // Pick up a first robot within range
-                if(rc.canPickUpUnit(robots[0].getID())){
-                    rc.pickUpUnit(robots[0].getID());
-                    System.out.println("I picked up an enemy bot");
-                    haveEnemyBot = true;
-                }else {
-                    System.out.println("Flying to enemey location");
-                    nav.flyTo(robots[0].location);
+                boolean enemyRobotFound = false;
+                int closestEnemyBotDistance = 9999;
+                RobotInfo closestEnemyBot = null;
+                for(int i = 0; i < robots.length && !haveEnemyBot; ++i){
+                    if(rc.canPickUpUnit(robots[i].getID())){
+                        rc.pickUpUnit(robots[i].getID());
+                        System.out.println("I picked up an enemy bot");
+                        haveEnemyBot = true;
+                    } else {
+                        if(isPickable(robots[i])){
+                            int distanceToEnemyBot = rc.getLocation().distanceSquaredTo(robots[i].location);
+                            if( distanceToEnemyBot < closestEnemyBotDistance){
+                                closestEnemyBot = robots[i];
+                                closestEnemyBotDistance = distanceToEnemyBot;
+                            }
+                        }
+                    }
+                }
+                if(!haveEnemyBot && closestEnemyBot != null){
+                    nav.flyTo(closestEnemyBot.location);
+                    System.out.println("Flying to location: " + closestEnemyBot.location);
                 }
             }else {
+                if(Math.random() < .8){
+                    nav.tryFly(rc.getLocation().directionTo(enemyHqSymetric));
+                }else {
+                    nav.tryFly(randomDirection());
+                }
 
                 System.out.println("No enemy units found");
             }
@@ -213,6 +235,10 @@ public class DeliveryDrone extends Unit{
         for(int [] message : currentBlockChainMessage){
             if (message[1] == 8) {
                 rush = true;
+            }else if (message[1] == 9){
+                locEnemyBot = new MapLocation(message[2], message[3]);
+            }else if (message[1] == 11){
+                floodedLocations.add(new MapLocation(message[2], message[3]));
             }
             // Set Enemy Hq Location
             else if(message[1] == 6){
