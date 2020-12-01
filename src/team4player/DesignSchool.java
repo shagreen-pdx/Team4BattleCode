319f6294b9b783aafe38e8ad8e78ef08ab0788ee
Yufei <46723525+YufeiT@users.noreply.github.com>
2020-12-01 01:29:19 -0800

    refactored and added more tests

--- src/team4player/DesignSchool.java
+++ src/team4player/DesignSchool.java
@@ -18,42 +18,42 @@ public class DesignSchool extends Building{
 
 
         System.out.println(numLandscapers);
-
-//        if(!broadcastedCreation){
-//            broadcastedCreation = comms.broadcastMessage(rc.getLocation(), 1);
-//        }
-
         if(!teamMessagesSearched){
             decipherAllBlockChainMessages();
         }
 
         decipherCurrentBlockChainMessage();
-
         if(rampUpProduction){
+            rampUp();
+        } else {
+            elseProduction();
+        }
+    }
+
+    public void rampUp() throws GameActionException {
+        for (Direction dir : Util.directions) {
+            if (tryBuild(RobotType.LANDSCAPER, dir)) {
+                numLandscapers++;
+                System.out.println("Created a new landscaper!");
+                if(numLandscapers == 5){
+                    rampUpProduction = false;
+                    comms.broadcastMessage(14, 1);
+                }
+            }
+        }
+        if(rc.getDirtCarrying() == 14){
+            comms.broadcastMessage(14, 1);
+        }
+    }
+
+    public void elseProduction() throws GameActionException {
+        if(canBuild && rc.getTeamSoup() > 210 && rc.isReady()) {
             for (Direction dir : Util.directions) {
                 if (tryBuild(RobotType.LANDSCAPER, dir)) {
                     numLandscapers++;
                     System.out.println("Created a new landscaper!");
-                    if(numLandscapers == 5){
-                        rampUpProduction = false;
-                        comms.broadcastMessage(14, 1);
-                    }
-                }
-            }
-            if(rc.getDirtCarrying() == 14){
-                comms.broadcastMessage(14, 1);
-            }
-        } else {
-            if(canBuild && rc.getTeamSoup() > 210) {
-                if (rc.isReady()) {
-                    for (Direction dir : Util.directions) {
-                        if (tryBuild(RobotType.LANDSCAPER, dir)) {
-                            numLandscapers++;
-                            System.out.println("Created a new landscaper!");
-                            canBuild = false;
-                            comms.broadcastMessage(rc.getLocation(),15);
-                        }
-                    }
+                    canBuild = false;
+                    comms.broadcastMessage(rc.getLocation(),15);
                 }
             }
         }
@@ -73,17 +73,22 @@ public class DesignSchool extends Building{
     public void decipherCurrentBlockChainMessage() throws GameActionException{
         ArrayList<int []> currentBlockChainMessage = comms.getPrevRoundMessages();
         for(int [] message : currentBlockChainMessage){
-            if(message[1] == 8 && message[4] == rc.getID()){
-                System.out.print("Recieved personal message");
-                rampUpProduction = true;
-                canBuild = true;
-            } else if (message[1] == 16 ){
-                MapLocation loc = new MapLocation(message[2], message[3]);
-                if(!loc.equals(rc.getLocation())){
-                    canBuild = true;
-                }
-
-            }
+            decipherCurrentBlockChainMessageHelper(message);
         }
     }
+
+    public int[] decipherCurrentBlockChainMessageHelper(int[] message) throws GameActionException {
+        if(message[1] == 8 && message[4] == rc.getID()){
+            System.out.print("Recieved personal message");
+            rampUpProduction = true;
+            canBuild = true;
+        } else if (message[1] == 16 ){
+            MapLocation loc = new MapLocation(message[2], message[3]);
+            if(!loc.equals(rc.getLocation())){
+                canBuild = true;
+            }
+        }
+        return message;
+    }
+
 }
--- test/team4player/DesignSchoolTest.java
+++ test/team4player/DesignSchoolTest.java
@@ -1,7 +1,12 @@
 package team4player;
 
 import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
+import battlecode.common.Team;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -13,24 +18,73 @@ import java.util.Arrays;
 import static org.mockito.Mockito.*;
 
 public class DesignSchoolTest {
-  @Mock
-  ArrayList<Integer> teamMessages;
-  @Mock
-  RobotController rc;
-  @Mock
-  Communications comms;
-  @InjectMocks
-  DesignSchool designSchool;
+    //Field enemyHqLoc of type MapLocation - was not mocked since Mockito doesn't mock a Final class when 'mock-maker-inline' option is not set
+    @Mock
+    ArrayList<int[]> teamMessages;
+    @Mock
+    RobotController rc;
+    @Mock
+    Communications comms;
+    @InjectMocks
+    DesignSchool designSchool;
 
-  @Before
-  public void setUp() {
-    MockitoAnnotations.initMocks(this);
-  }
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
+    @Test(expected = NullPointerException.class)
+    public void testTakeTurn() throws Exception {
+        when(comms.broadcastMessage(any(), anyInt())).thenReturn(true);
+        when(comms.broadcastMessage(anyInt(), anyInt())).thenReturn(true);
+        when(comms.getPrevRoundMessages()).thenReturn(new ArrayList<>(Arrays.asList(new int[]{0})));
 
-  @Test
-  public void testDecipherAllBlockChainMessages() throws Exception {
-    designSchool.decipherAllBlockChainMessages();
-  }
+        designSchool.takeTurn();
+    }
 
+    @Test
+    public void testRampUp() throws Exception {
+        when(comms.broadcastMessage(anyInt(), anyInt())).thenReturn(true);
+
+        designSchool.rampUp();
+    }
+
+    @Test
+    public void testElseProduction() throws Exception {
+        when(comms.broadcastMessage(any(), anyInt())).thenReturn(true);
+
+        designSchool.elseProduction();
+    }
+
+    @Test
+    public void testDecipherAllBlockChainMessages() throws Exception {
+        designSchool.decipherAllBlockChainMessages();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testDecipherCurrentBlockChainMessage() throws Exception {
+        when(comms.getPrevRoundMessages()).thenReturn(new ArrayList<>(Arrays.asList(new int[]{0})));
+
+        designSchool.decipherCurrentBlockChainMessage();
+    }
+
+    @Ignore
+    @Test
+    public void testDecipherCurrentBlockChainMessageHelper() throws Exception {
+        int[] result = designSchool.decipherCurrentBlockChainMessageHelper(new int[]{0});
+        //Assert.assertArrayEquals(new int[]{0}, result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testIsNearbyRobot() throws Exception {
+        boolean result = designSchool.isNearbyRobot(new RobotInfo[]{null}, RobotType.HQ);
+        Assert.assertEquals(true, result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testIsNearbyRobot2() throws Exception {
+        boolean result = designSchool.isNearbyRobot(new RobotInfo[]{null}, RobotType.HQ, Team.A);
+        Assert.assertEquals(true, result);
+    }
 }
+
