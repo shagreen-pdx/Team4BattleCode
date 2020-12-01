449227e9ea183b4af8d259bf01c9a2db4fb3717e
Shay Green <shagreen@pdx.edu>
2020-11-30 18:09:00 -0800

    Design school refactoring and unit tests

--- test/team4player/DesignSchoolTest.java
+++ test/team4player/DesignSchoolTest.java
@@ -1,6 +1,7 @@
 package team4player;
 
 import battlecode.common.RobotController;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -19,8 +20,11 @@ public class DesignSchoolTest {
   RobotController rc;
   @Mock
   Communications comms;
+  @Mock
+  Robot robot;
   @InjectMocks
   DesignSchool designSchool;
+
 
   @Before
   public void setUp() {
@@ -31,6 +35,36 @@ public class DesignSchoolTest {
   @Test
   public void testDecipherAllBlockChainMessages() throws Exception {
     designSchool.decipherAllBlockChainMessages();
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testDecipherCurrentBlockChainMessages() throws Exception {
+    when(comms.getPrevRoundMessages()).thenReturn(new ArrayList<int[]>(Arrays.asList(new int[]{0})));
+
+    designSchool.decipherCurrentBlockChainMessage();
+  }
+
+  @Test
+  public void testRampingUpProduction() throws Exception {
+    when(robot.tryBuild(any(), any())).thenReturn(true);
+    when(rc.getDirtCarrying()).thenReturn(14);
+
+    designSchool.rampingUpProduction();
+  }
+
+  @Test
+  public void testCreatingNewLandscaper() throws Exception {
+    designSchool.numLandscapers = 1;
+    designSchool.canBuild = true;
+    when(rc.getTeamSoup()).thenReturn(211);
+    when(rc.isReady()).thenReturn(true);
+    when(robot.tryBuild(any(), any())).thenReturn(true);
+    when(comms.broadcastMessage(any(), anyInt())).thenReturn(true);
+
+    designSchool.creatingNewLandscaper();
+
+    Assert.assertEquals(1, designSchool.numLandscapers);
+    Assert.assertEquals(true,designSchool.canBuild);
   }
 
 }
