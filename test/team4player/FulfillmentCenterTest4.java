d54357331ea26c298098389b90300ebf3ba5407a
Shay Green <shagreen@pdx.edu>
2020-11-30 16:11:59 -0800

    Fulfillment center test

--- test/team4player/FulfillmentCenterTest4.java
+++ test/team4player/FulfillmentCenterTest4.java
@@ -2,11 +2,14 @@ package team4player;
 
 import battlecode.common.Direction;
 import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
@@ -21,6 +24,8 @@ public class FulfillmentCenterTest4 {
     RobotController rc;
     @Mock
     Communications comms;
+    @Mock
+    Robot robot;
     @InjectMocks
     FulfillmentCenter fulfillmentCenter;
 
@@ -29,14 +34,23 @@ public class FulfillmentCenterTest4 {
         MockitoAnnotations.initMocks(this);
     }
 
-    @Ignore
     @Test(expected = NullPointerException.class)
     public void testTakeTurn() throws Exception {
         comms.broadcastedCreation =true;
         fulfillmentCenter.canBuild = true;
         
         when(comms.broadcastMessage(any(), anyInt())).thenReturn(true);
-        when(comms.broadcastMessage(anyInt(), anyInt(), 1)).thenReturn(true);
+        when(comms.getPrevRoundMessages()).thenReturn(new ArrayList<int[]>(Arrays.asList(new int[]{0})));
+
+        fulfillmentCenter.takeTurn();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testTakeTurn2() throws Exception {
+        comms.broadcastedCreation =false;
+        fulfillmentCenter.canBuild = true;
+
+        when(comms.broadcastMessage(any(), anyInt())).thenReturn(true);
         when(comms.getPrevRoundMessages()).thenReturn(new ArrayList<int[]>(Arrays.asList(new int[]{0})));
 
         fulfillmentCenter.takeTurn();
@@ -47,5 +61,41 @@ public class FulfillmentCenterTest4 {
         when(comms.getPrevRoundMessages()).thenReturn(new ArrayList<int[]>(Arrays.asList(new int[]{0})));
 
         fulfillmentCenter.decipherCurrentBlockChainMessage();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testDroneBuilt() throws Exception{
+        when(comms.broadcastMessage(any(), anyInt())).thenReturn(true);
+        int num = fulfillmentCenter.numDeliveryDrones;
+
+        fulfillmentCenter.droneBuilt();
+        System.out.println("Test");
+        Assert.assertEquals(false, fulfillmentCenter.canBuild);
+        Assert.assertEquals(num + 1, fulfillmentCenter.numDeliveryDrones);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testBroadcastDroneID() throws Exception{
+        when(comms.broadcastMessage(any(), anyInt())).thenReturn(true);
+
+        Direction dir = Util.randomDirection();
+        fulfillmentCenter.broadcastDroneID(dir);
+    }
+
+    @Test
+    public void testTryBuildDrone() throws Exception{
+        when(comms.broadcastMessage(any(), anyInt())).thenReturn(true);
+        when(robot.tryBuild(any(), any())).thenReturn(true);
+
+        fulfillmentCenter.tryBuildDrone();
+    }
+
+    @Test
+    public void testTryBuildDrone2() throws Exception{
+        fulfillmentCenter.numDeliveryDrones = -1;
+        when(robot.tryBuild(any(), any())).thenReturn(true);
+        when(comms.broadcastMessage(any(), anyInt())).thenReturn(true);
+
+        fulfillmentCenter.tryBuildDrone();
     }
 }
